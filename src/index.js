fabfc41e764205f7224f298ce64cd5efe0210ec3
Jeffrey Lonergan <jrlone1990@gmail.com>
2018-05-29 20:47:40 -0700

    got webpack to run, printing react component to index.html

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const component = <div>Yo Yo World</div>;
+
+ReactDOM.render(
+  component,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/* eslint-env node */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //run babel from this file, change all other types to javaScript
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
